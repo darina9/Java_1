6a6510b4be60d396f30da7f2efc8de8321e84f0f
Darina <krastinadv@gmail.com>
2022-12-09 15:15:10 +0300

    DZ_1

---
+++ DZ_1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class DZ_1 {
+    public static void main(String[] args) {
+        Scanner st = new Scanner(System.in);
+        System.out.print("Введите n: ");
+        int n = st.nextInt();
+        st.close();
+        int t = n* (n+1)/2;
+        System.out.println(t); 
+    }
+    }
+
+
+            
+    
+     
+    
+
+
+
+
+
+
+
+
+
